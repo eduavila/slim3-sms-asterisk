796cb4975f6a6351352e2d887da1c2e2f0dfb15e
root <root@localhost.localdomain>
2016-12-20 18:00:42 -0300

    fix warning do time

--- app/dependencies.php
+++ app/dependencies.php
@@ -7,7 +7,7 @@ use App\Models\Campanha;
 // 
 
 setlocale(LC_ALL, 'pt_BR', 'pt_BR.utf-8', 'pt_BR.utf-8', 'portuguese');
-
+date_default_timezone_set('America/Cuiaba');
 
 $container = $app->getContainer();
 
